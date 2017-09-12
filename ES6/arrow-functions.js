1785fd0582c075d336186a5c5ce7145fd7343f14
Balakrishna Ragala <baluragala@gmail.com>
2017-09-12 21:52:54 +0530

    added example for scope and this

--- ES6/arrow-functions.js
+++ ES6/arrow-functions.js
@@ -55,3 +55,24 @@ let divisibleBy3 = numbers.filter(function (number) {
 })
 
 let divisibleBy2 = numbers.filter((number) => number % 2 == 0);
+
+
+
+// understand scope and this
+
+let Person = function(name,age){
+	this.name = name;
+	this.age = age;
+	this.print = function(){
+		
+		console.log(`Name :${this.name} and Age: ${this.age}`)
+	}
+}
+
+let p = new Person('James',32)
+
+let print = p.print;
+
+setTimeout(function(){
+	print()
+},1000)
